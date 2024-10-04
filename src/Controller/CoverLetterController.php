e1c47486bdb4a22f24246313947b65eb0cfa9371
Matheo93 <Math.be2015@gmail.com>
2024-10-04 18:56:21 +0200

    last push

--- src/Controller/CoverLetterController.php
+++ src/Controller/CoverLetterController.php
@@ -1,18 +1,42 @@
 <?php
 
+// src/Controller/CoverLetterController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoverLetterController extends AbstractController
 {
-    #[Route('/cover/letter', name: 'app_cover_letter')]
-    public function index(): Response
+    #[Route('/generate-letter', name: 'generate_letter', methods: ['POST'])]
+    public function generateLetterPost(Request $request)
     {
-        return $this->render('cover_letter/index.html.twig', [
-            'controller_name' => 'CoverLetterController',
+        $recipientName = $request->request->get('recipient_name');
+        $jobPosition = $request->request->get('job_position');
+        $motivations = $request->request->get('motivations');
+
+        $user = $this->getUser();
+        $userName = $user->getFirstName() . ' ' . $user->getLastName();
+
+        // Générer la lettre
+        $letterContent = "
+        Cher/Chère $recipientName,
+
+        Je me permets de vous contacter afin de postuler pour le poste de $jobPosition. 
+
+        Mes motivations sont les suivantes : $motivations.
+
+        En espérant pouvoir vous rencontrer prochainement.
+
+        Cordialement,
+
+        $userName
+        ";
+
+        return $this->render('cover_letter/result.html.twig', [
+            'letterContent' => nl2br($letterContent)
         ]);
     }
 }
